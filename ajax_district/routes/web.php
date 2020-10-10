8ecfcc2874958eadbf63b187895fe89c2f8ddd6c
ruhul <alfa@gmail>
2020-10-10 16:48:30 +0600

    dropdown menu

---
+++ ajax_district/routes/web.php
@@ -0,0 +1,6 @@
+<?php
+
+Route::get('dropdownlist','DropdownController@index');
+Route::get('get-state-list','DropdownController@getStateList');
+Route::get('get-city-list','DropdownController@getCityList');
+
